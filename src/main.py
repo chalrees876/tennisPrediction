c1e75f73e3bc6a659a66a545f5997f32cd800113
chalrees876 <chris.mcke876@gmail.com>
2025-08-18 18:48:05 -0400

    migrated serve parsing to point class

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from src.Player import Player
 from src.Players import Players
+import src.codes as codes
+from src.Point import Point
 
 
 def main():
@@ -25,56 +27,6 @@ def main():
                      'Notes': "string",
                      'PtWinner': "Int16"}
 
-    serve = {"4": "wide",
-             "5": "body",
-             "6": "down the t"}
-
-    serve_return_depth = {
-        "7": "inside service line",
-        "8": "behind service line",
-        "9": "deep",
-        "0": "unknown"
-    }
-
-    shot = {"f": "forehand",
-            "b": "backhand",
-            "s": "backhand slice",
-            "r": "forehand slice",
-            "v": "forehand volley",
-            "z": "backhand volley",
-            "o": "overhead",
-            "p": "backhand overhead smash",
-            "u": "forehand drop shot",
-            "y": "backhand drop shot",
-            "l": "forehand lob",
-            "m": "backhand lob",
-            "h": "forehand half-volley",
-            "i": "backhand half-volley",
-            "j": "forehand swinging volley",
-            "k": "backhand swinging volley",
-            "t": "trick shot",
-            "q": "unknown"}
-
-    shot_direction = {"1": "deuce side",
-                      "2": "down middle",
-                      "3": "ad side"}
-
-    point_end = {"@": "unforced error",
-                 "#": "forced error",
-                 "*": "winner"}
-
-    error = {"n": "net",
-             "w": "wide",
-             "d": "deep",
-             "x": "wide and deep",
-             "!": "shank",
-             "e": "unknown"}
-
-    optional = {"+": "approach shot",
-                   "-": "net",
-                   "=": "baseline",
-                "^": "drop volley"}
-
     df = pd.read_csv('../data/charting-m-points-2020s.csv', dtype=dtypes)
 
     df[['1st', '2nd']] = df[['1st', '2nd']].replace({'c':''}, regex=True)
@@ -92,21 +44,33 @@ def main():
     player2 = Player(player2_first_name, player2_last_name)
     players.add(player2)
 
+    point = Point(fo_final['Pt'])
+
     player1_serving = fo_final.loc[fo_final['Svr'] == 1]
     player2_serving = fo_final.loc[fo_final['Svr'] == 2]
 
     player1_serving_points = player1_serving[['1st', '2nd', 'PtWinner']]
-    s1 = player1_serving_points['1st'].fillna("").astype(str)
-    s2 = player1_serving_points['2nd'].fillna("").astype(str)
+    p1_first_serve = player1_serving_points['1st'].fillna("").astype(str)
+    p1_second_serve = player1_serving_points['2nd'].fillna("").astype(str)
 
-    player1.handle_serve(s1, s2, serve, shot, error)
-
-    for serve in player1.serves:
-        print(serve)
-
+    player2_serving_points = player2_serving[['1st', '2nd', 'PtWinner']]
+    p2_first_serve = player2_serving_points['1st'].fillna("").astype(str)
+    p2_second_serve = player2_serving_points['2nd'].fillna("").astype(str)
 
 
 
+    player1.handle_serve(p1_first_serve, p1_second_serve, codes.serve_code, codes.shot_code, codes.error_code)
+    player2.handle_serve(p2_first_serve, p2_second_serve, codes.serve_code, codes.shot_code, codes.error_code)
+
+    print(player1.first_name, player1.last_name, "first serve percentage:", player1.first_serve_percent)
+    print(player1.first_name, player1.last_name, "second serve percentage:", player1.second_serve_percent)
+
+    print(player2.first_name, player2.last_name, "first serve percentage:", player2.first_serve_percent)
+    print(player2.first_name, player2.last_name, "second serve percentage:", player2.second_serve_percent)
+
+
+    print(player1.full_name(), "aces:", player1.aces, "double faults:", player1.double_faults)
+    print(player2.full_name(), "aces:", player2.aces, "double faults:", player2.double_faults)
 
 
 def parse_match_id(match_id_string):
