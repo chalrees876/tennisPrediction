17972e58a369cc6978262b1a0bb6932566ba9180
chalrees876 <chris.mcke876@gmail.com>
2025-08-19 23:35:34 -0400

    working on serve analytics

--- src/serveAnalytics.py
+++ src/serveAnalytics.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import main
 
 #create serve dataframe
 #record stats of any match that is input
+columns = ["Serve Direction", "Serve Num", "Is Fault", "Miss Type", "Is Ace", "SNV", "Match Id", "Point Id", "Set Score", "Game Score", "Point Score", "Server", "Returner", "Point Winner"]
+serve_events = main.main()
+
+serve_events.print()
+
+df = pd.DataFrame(serve_events, columns=columns)
+
