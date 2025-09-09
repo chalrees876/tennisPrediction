846174f031a9a786f669ba6ab595d6da3489d933
chalrees876 <chris.mcke876@gmail.com>
2025-09-09 16:42:16 -0400

    created django model classes

--- tennis/views.py
+++ tennis/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-
-from .models import Player
+from .models import Player, Tournament, Match
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
 def home(request):
     players = Player.objects.all()
-    return render(request, 'tennis/home.html', {'players': players})
+    tournaments = Tournament.objects.all()
+    matches = Match.objects.all()
+    return render(request, 'tennis/home.html', {'players': players, 'tournaments': tournaments, 'matches': matches})
+
