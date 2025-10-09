ab2ae3f4c2c0ed64a4c681dae3c6ece5d6963a90
chalrees876 <chris.mcke876@gmail.com>
2025-10-09 12:07:52 -0400

    editing prod settings

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -15,10 +15,10 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "myproject"),
-        "USER": os.getenv("DB_USER", "myprojectuser"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "password"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
