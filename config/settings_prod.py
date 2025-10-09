76418b43b1f0ab3df5c5678b1a539a916d5eb23c
chalrees876 <chris.mcke876@gmail.com>
2025-10-09 13:39:17 -0400

    editing prod settings

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -1,7 +1,7 @@
 import os
 from .settings_base import *
 
-DEBUG = False
+DEBUG = True
 import os
 
 def _split_env(name):
@@ -21,4 +21,47 @@ DATABASES = {
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT"),
     }
+}
+
+LOG_DIR = "/var/log/tennisapp"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+        },
+    },
+    "handlers": {
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": os.path.join(LOG_DIR, "django.log"),
+            "maxBytes": 5_000_000,
+            "backupCount": 5,
+            "formatter": "standard",
+        },
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "root": {
+        "handlers": ["file", "console"],
+        "level": os.environ.get("DJANGO_LOG_LEVEL", "INFO"),
+    },
+    "loggers": {
+        "django.request": {  # uncaught exceptions in views end up here
+            "handlers": ["file", "console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+        # your project/app logs:
+        "tennis": {
+            "handlers": ["file", "console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
 }
