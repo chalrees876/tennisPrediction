32ab7f055cc1aa0c048097c503bcae5aaa2ecbaf
chalrees876 <chris.mcke876@gmail.com>
2026-01-07 22:11:55 -0500

    updating prod settings

--- config/settings_dev.py
+++ config/settings_dev.py
@@ -7,8 +7,6 @@ ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 SECRET_KEY = "dev-secret-key-not-for-production"
 
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://neondb_owner:npg_7nfYRuq8GiTZ@ep-proud-tooth-ahsdsq47-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require')
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
